00883781b0658054c8e9b80ebc478f8674af9697
Kaya Baber <Kaya.Baber@gmail.com>
2016-05-06 12:18:23 -0700

    made two perturbed sets of potentials, add plotting

--- Assignment_4_quantum_simulation/P440_Assign4_Exp2.py
+++ Assignment_4_quantum_simulation/P440_Assign4_Exp2.py
@@ -8,6 +8,7 @@ Exploration 1
 import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
+import math
 
 def make_banded(N):
     N-=2                                 #we reduce N by 2 because we aren't using the outer ring of indicies
@@ -36,14 +37,14 @@ def normalize(psi):
 def V(x,V0):                            #potential function
     #V'(x) = V_0*exp(-256*x^2)
     #V_0 = E_0 / 10.0
-    vOut = V0*exp(-256*(x**2))
+    vOut = V0*math.exp(-256*(x**2))
     return vOut
 
 
 def perturb(psi,x,V,V0):
     #integrate over x to get delta_E
     psiSqr = psi**2                     #square normalized eigan vector points
-    for i in len(psi):                   
+    for i in range(len(psi)):                   
         psiSqr[i] = psiSqr[i]*V(x[i],V0)    #multiply squared points by V'(x)
     #use trap rule to integrate over x
     psiSqrSum = np.sum(psiSqr) / (2.* (len(psi)-1) ) #sum up all points and divide by 2 times number of intervals 2(N-1)
@@ -53,8 +54,8 @@ def perturb(psi,x,V,V0):
 
 N=100
 banded = make_banded(N)
-
 x=np.linspace(-0.5,0.5,N)               #positions
+xArraySub=x[1:-1]
 
 eiganVal, eiganVect=LA.eig(banded)      #compute eigan vectors and values
 
@@ -62,7 +63,7 @@ numVectors = 25
 
 psiArray=[]
 deltaEArray=[]
-for i in range(numVectors):            #constructs array of eigan vectors
+for i in range(numVectors):             #constructs array of eigan vectors
     psi=np.insert(eiganVect[:,i],0 ,0)
     psi=np.append(psi,0)
     psi=normalize(psi)
@@ -73,11 +74,66 @@ for p in psiArray:
     psiNorm = normalize(p)
     psiNormArray.append(psiNorm)
     
-print eiganVal[:numVectors]
-#print out the delta_E values
-#add the delta_E values to the original eigan energies
-#print out the new eigan energies
-
+    
     
 
+V0=eiganVal[2] / 10.0
+deltaEArray
+for p in psiNormArray:
+    deltaE=perturb(p,x,V,V0)
+    deltaEArray.append(deltaE)
+print eiganVal[:numVectors]
+newEnergy = np.add(deltaEArray,eiganVal[:numVectors])
+print np.array(newEnergy)
 
+
+vArray=[]
+for i in xArraySub:
+    vArray.append(V(i,V0))
+Vdiag=np.diag(vArray)
+H = np.add(Vdiag,banded)
+eiganValH, eiganVect=LA.eig(H)
+psiArray=[]
+for i in range(numVectors):             #constructs array of eigan vectors
+    psi=np.insert(eiganVect[:,i],0 ,0)
+    psi=np.append(psi,0)
+    psi=normalize(psi)
+    psiArray.append(psi)
+    plt.plot(x, psi)
+    plt.ylabel("Eigan Vector")
+    plt.xlabel("Position (x)")
+    plt.grid()
+    plt.savefig('Assignment_4_quantum_simulation/plots/2AeiganVect'+str(i)+'.png',bbox_inches='tight')
+    #plt.show()
+    plt.clf()
+
+
+
+
+V0=eiganVal[2]
+deltaEArray=[]
+for p in psiNormArray:
+    deltaE=perturb(p,x,V,V0)
+    deltaEArray.append(deltaE)
+newEnergy = np.add(deltaEArray,eiganVal[:numVectors])
+print np.array(newEnergy)
+
+vArray=[]
+for i in xArraySub:
+    vArray.append(V(i,V0))
+Vdiag=np.diag(vArray)
+H = np.add(Vdiag,banded)
+eiganValH, eiganVect=LA.eig(H)
+psiArray=[]
+for i in range(numVectors):             #constructs array of eigan vectors
+    psi=np.insert(eiganVect[:,i],0 ,0)
+    psi=np.append(psi,0)
+    psi=normalize(psi)
+    psiArray.append(psi)
+    plt.plot(x, psi)
+    plt.ylabel("Eigan Vector")
+    plt.xlabel("Position (x)")
+    plt.grid()
+    plt.savefig('Assignment_4_quantum_simulation/plots/2BeiganVect'+str(i)+'.png',bbox_inches='tight')
+    #plt.show()
+    plt.clf()
