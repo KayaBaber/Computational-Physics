66d923c6d08362b9e015d3575a099a94c7622d92
Kaya Baber <Kaya.Baber@gmail.com>
2016-05-06 11:16:17 -0700

    Completed perturb function

--- Assignment_4_quantum_simulation/P440_Assign4_Exp2.py
+++ Assignment_4_quantum_simulation/P440_Assign4_Exp2.py
@@ -9,30 +9,8 @@ import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
 
-#continue from exploration 1
-#normalize eigan vectors
-    #square eigan vector points
-    #use trap rule to integrate over x
-        #sum up all points
-        #divide by 2 times number of intervals 2(N-1)
-    #take square root of integral
-    #divide all eigan vector points by the square root
-#integrate over x to get delta_E
-    #square normalized eigan vector points
-    #multiple squared points by V'(x)
-        #V'(x) = V_0*exp(-256*x^2)
-        #V_0 = E_0 / 10.0
-    #use trap rule to integrate over x
-        #sum up all points
-        #divide by 2 times number of intervals 2(N-1)
-#print out the original eigan energies
-#print out the delta_E values
-#add the delta_E values to the original eigan energies
-#print out the new eigan energies
-
-
 def make_banded(N):
-    N-=2    #we reduce N by 2 because we aren't using the outer ring of indicies
+    N-=2                                 #we reduce N by 2 because we aren't using the outer ring of indicies
     stepSize = 1.0/N  
     bandTopBot = [1.0/ (stepSize**2)]*(N-1)
     bandMid = [-2.0/ (stepSize**2)]*N
@@ -41,40 +19,50 @@ def make_banded(N):
     banded = np.add(banded,np.diag(bandTopBot,1))
     banded = np.add(banded,np.diag(bandTopBot,-1))
     return banded
+#continue from exploration 1
 
 
+#normalize eigan vectors
 def normalize(psi):
     #takes in eigan vector and normalizes it
-    psiSqr = psi**2         
-    psiSqrSum = np.sum(psiSqr) / (2.* (len(psi)-1) )
-    normConst = psiSqrSum ** (1./2.)
-    psiNorm = psi / normConst
+    psiSqr = psi**2                     #square eigan vector points
+    #use trap rule to integrate over x
+    psiSqrSum = np.sum(psiSqr) / (2.* (len(psi)-1) ) #sum up all points and divide by 2 times number of intervals 2(N-1)
+    normConst = psiSqrSum ** (1./2.)    #take square root of integral
+    psiNorm = psi / normConst           #divide all eigan vector points by the square root
     return psiNorm
 
-def V(x,V0):
+
+def V(x,V0):                            #potential function
+    #V'(x) = V_0*exp(-256*x^2)
+    #V_0 = E_0 / 10.0
     vOut = V0*exp(-256*(x**2))
     return vOut
 
 
-def perturb(psi,x,V(x,V0)):
-    psiSqr = psi**2
-    for pS in psiSqr:
-        pS = s
-    
+def perturb(psi,x,V,V0):
+    #integrate over x to get delta_E
+    psiSqr = psi**2                     #square normalized eigan vector points
+    for i in len(psi):                   
+        psiSqr[i] = psiSqr[i]*V(x[i],V0)    #multiply squared points by V'(x)
+    #use trap rule to integrate over x
+    psiSqrSum = np.sum(psiSqr) / (2.* (len(psi)-1) ) #sum up all points and divide by 2 times number of intervals 2(N-1)
+    deltaE=psiSqrSum                    #the change in energy is the integral
+    return deltaE
 
 
 N=100
 banded = make_banded(N)
 
-x=np.linspace(-0.5,0.5,N)           #positions
+x=np.linspace(-0.5,0.5,N)               #positions
 
-eiganVal, eiganVect=LA.eig(banded)  #compute eigan vectors and values
+eiganVal, eiganVect=LA.eig(banded)      #compute eigan vectors and values
 
-num_vectors = 25
+numVectors = 25
 
 psiArray=[]
 deltaEArray=[]
-for i in range(num_vectors):        #constructs array of eigan vectors
+for i in range(numVectors):            #constructs array of eigan vectors
     psi=np.insert(eiganVect[:,i],0 ,0)
     psi=np.append(psi,0)
     psi=normalize(psi)
@@ -85,8 +73,10 @@ for p in psiArray:
     psiNorm = normalize(p)
     psiNormArray.append(psiNorm)
     
-
-
+print eiganVal[:numVectors]
+#print out the delta_E values
+#add the delta_E values to the original eigan energies
+#print out the new eigan energies
 
     
 
