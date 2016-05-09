996b519b21af8cff85337e9af14a62e3f642b699
Kaya Baber <Kaya.Baber@gmail.com>
2016-05-09 11:54:04 -0700

    initial commit of assignment 5 Exp 1

---
+++ Assignment_5_partial_differentials/P440_Assign5_Exp1.py
@@ -0,0 +1,11 @@
+'''
+Kaya Baber
+Physics 440 - Computational Physics
+Assignment 5 - PDEs
+Exploration 1 - Parabolic PDEs: Thermal Diffusion
+'''
+
+import numpy as np
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
+import math
