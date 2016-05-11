30c9359e33f6ec85ffad227dd8b68f3352f92c36
Kaya Baber <Kaya.Baber@gmail.com>
2016-05-11 11:15:13 -0700

    Set up  the procedure to code out

--- Assignment_5_partial_differentials/P440_Assign5_Exp1.py
+++ Assignment_5_partial_differentials/P440_Assign5_Exp1.py
@@ -9,3 +9,26 @@ import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
 import math
+
+
+
+
+#make banded matrix 
+#initialize column vector 
+#matrix multiply
+#modifiy boundaries
+#repeat
+
+
+
+def make_banded(N,M):
+    bandTopBot = [2 + (4*M)/(N**2) ]*(N-1)
+    bandMid = [-2.0/ (stepSize**2)]*N
+    
+    banded = np.diag(bandMid)
+    banded = np.add(banded,np.diag(bandTopBot,1))
+    banded = np.add(banded,np.diag(bandTopBot,-1))
+    return banded
+    
+    
+#banded.dot(thermalArray)
