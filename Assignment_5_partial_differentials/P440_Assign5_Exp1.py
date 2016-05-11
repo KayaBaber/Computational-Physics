2aa8bc4b7818eec449f9c95b0757fb34511e3a0a
Kaya Baber <Kaya.Baber@gmail.com>
2016-05-11 11:29:44 -0700

    Fixed the procedure to code out

--- Assignment_5_partial_differentials/P440_Assign5_Exp1.py
+++ Assignment_5_partial_differentials/P440_Assign5_Exp1.py
@@ -22,13 +22,17 @@ import math
 
 
 def make_banded(N,M):
-    bandTopBot = [2 + (4*M)/(N**2) ]*(N-1)
-    bandMid = [-2.0/ (stepSize**2)]*N
+    bandTopBot = [-1.]*(N-1)
+    bandMid = [2. + (4.*M)/(N**2) ]*N
     
     banded = np.diag(bandMid)
     banded = np.add(banded,np.diag(bandTopBot,1))
     banded = np.add(banded,np.diag(bandTopBot,-1))
     return banded
     
-    
+
+N=10
+M=100
+bandedCrank = make_banded(N,M)
+print bandedCrank
 #banded.dot(thermalArray)
