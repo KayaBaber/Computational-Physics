af5182b0a83d6bdb96fd9373a56b9055328fb4d8
Kaya Baber <Kaya.Baber@gmail.com>
2016-05-13 11:12:30 -0700

    wrote procedure to solve problem

--- Assignment_5_partial_differentials/P440_Assign5_Exp2.py
+++ Assignment_5_partial_differentials/P440_Assign5_Exp2.py
@@ -10,8 +10,18 @@ from numpy import linalg as LA
 import matplotlib.pyplot as plt
 import math
 
-
-
+#make initial velocity array in real space
+#make initial density array in real space
+#fft both to fourier space
+#make a column vector of density_f appended to velocity_f
+#set the x range to (0->2pi)
+#make derivative operator matrix
+    #i(diag (0,1,2,3,4,.. -4, -3, -2, -1)), where i is imaginary i
+#make quad matrix [[I][op],[op][I]] and negative quad [[I][-op],[-op][I]]
+#matrix multiply the negative quad by the FFT column vector
+#linear algebra solve the pos_quad*newFFTvector = above-result for newFFTvector
+#make seperate copies of the 
+#inverse FFT newFFTvector and append the real parts of velocity and density
 def make_banded(N,M):
     bandTopBot = [-1.]*(N-1)
     bandMid = [2. + (4.*M)/(N**2) ]*N
@@ -30,3 +40,5 @@ def make_operator(N,M):
     operatorCrank = invertedCrank.dot(negativeCrank)
     return operatorCrank
 
+
+ 
